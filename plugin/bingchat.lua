ec1227bdcbcc027eee10d049e83002b7dc7bd075
François Hissel <francois.hissel@gmail.com>
2023-06-18 20:02:48 +0200

    Plugin file

--- plugin/bingchat.lua
+++ plugin/bingchat.lua
@@ -1,9 +1,1 @@
-" Title:        BingChat plugin
-" Description:  A plugin to chat with GPT from inside Neovim
-" Last Change:  June 17th, 2023
-" Maintainer:   François Hissel
-
-" Prevents the plugin from being loaded multiple times. If the loaded
-" variable exists, do nothing more. Otherwise, assign the loaded
-" variable and continue running this instance of the plugin.
 require 'bingchat.nvim'
